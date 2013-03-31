4c619b51467cc1c9cd12979f5a2f1197f610bb72
ushi <ushi@honkgong.info>
2013-03-31 13:50:11 +0200

    Documentation.

--- src/viscum/mailer/mail.go
+++ src/viscum/mailer/mail.go
@@ -9,11 +9,12 @@ import (
 )
 
 type Mail struct {
-  *db.Entry
-  From    string
-  headers map[string]string
+  *db.Entry                   // The entry to mail
+  From      string            // Sender address
+  headers   map[string]string // Mail headers
 }
 
+// Returns a new mail.
 func NewMail(e *db.Entry, from string) *Mail {
   return &Mail{
     Entry: e,
@@ -28,10 +29,14 @@ func NewMail(e *db.Entry, from string) *Mail {
   }
 }
 
+// Sets a header. Overrides existing header with the same name.
 func (self *Mail) SetHeader(k string, v string) {
   self.headers[textproto.CanonicalMIMEHeaderKey(k)] = v
 }
 
+// Returns a already set header.
+//
+// Returns an empty string, if the header is not already set.
 func (self *Mail) GetHeader(k string) string {
   if v, ok := self.headers[textproto.CanonicalMIMEHeaderKey(k)]; ok {
     return v
@@ -39,6 +44,7 @@ func (self *Mail) GetHeader(k string) string {
   return ""
 }
 
+// Writes headers to a io.Writer.
 func (self *Mail) WriteHeaders(w io.Writer) error {
   for k, v := range self.headers {
     if _, err := w.Write([]byte(k + ": " + v + "\r\n")); err != nil {
@@ -48,6 +54,7 @@ func (self *Mail) WriteHeaders(w io.Writer) error {
   return nil
 }
 
+// Writes the body to a io.Writer.
 func (self *Mail) WriteBody(w io.Writer) error {
   if _, err := fmt.Fprintf(w, "%s\n\n%s\n%s\n\n", self.FeedTitle, self.Title, self.Url); err != nil {
     return err
