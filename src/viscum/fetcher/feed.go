b9e5917f09fd443349196977f27891029dea25c4
ushi <ushi@honkgong.info>
2013-04-06 18:52:53 +0200

    Cleanup fetcher/feed.

--- src/viscum/fetcher/feed.go
+++ src/viscum/fetcher/feed.go
@@ -7,18 +7,23 @@ import (
   . "viscum/util"
 )
 
+const (
+  NS_ATOM = "http://www.w3.org/2005/Atom" // ATOM namespace
+  NS_RSS  = "*"                           // RSS namespace
+)
+
 type Feed struct {
   *db.Feed
-  entries      map[string]bool
-  entryHandler func(*db.Entry) error
+  entries      map[string]bool       // Set of already processed entries
+  entryHandler func(*db.Entry) error // Handles new entries.
 }
 
-//
+// Returns a new feed.
 func NewFeed(f *db.Feed, h func(*db.Entry) error) *Feed {
   return &Feed{Feed: f, entries: make(map[string]bool), entryHandler: h}
 }
 
-//
+// Fetches the feed and sends new entries to the handler.
 func (self *Feed) Fetch() error {
   doc := xmlx.New()
 
@@ -26,7 +31,7 @@ func (self *Feed) Fetch() error {
     return err
   }
 
-  if n := doc.SelectNode("http://www.w3.org/2005/Atom", "feed"); n != nil {
+  if n := doc.SelectNode(NS_ATOM, "feed"); n != nil {
     self.processAtom(n)
     return nil
   }
@@ -35,44 +40,42 @@ func (self *Feed) Fetch() error {
     self.processRss(n)
     return nil
   }
-
   return errors.New("Unsupported Format")
 }
 
-//
-func (self *Feed) processAtom(node *xmlx.Node) {
-  ns := "http://www.w3.org/2005/Atom"
-  self.Title = Titleize(node.S(ns, "title"))
+// Sends new entries to the handler and registers the url.
+func (self *Feed) registerEntry(url string, title string, body string) {
+  e := &db.Entry{Url: url, Title: Titleize(title), Body: body}
 
-  for _, entry := range node.SelectNodes(ns, "entry") {
-    url := self.extractAtomUrl(ns, entry)
+  if err := self.entryHandler(e); err != nil {
+    Error("[Feed]", err)
+  } else {
+    self.entries[e.Url] = true
+  }
+}
+
+// Processes ATOM feeds.
+func (self *Feed) processAtom(node *xmlx.Node) {
+  self.Title = Titleize(node.S(NS_ATOM, "title"))
+
+  for _, entry := range node.SelectNodes(NS_ATOM, "entry") {
+    url := self.extractAtomUrl(entry)
 
     if len(url) == 0 || self.entries[url] {
       continue
     }
+    body := entry.S(NS_ATOM, "content")
 
-    e := &db.Entry{
-      Url:   url,
-      Title: Titleize(entry.S(ns, "title")),
-      Body:  entry.S(ns, "content"),
+    if len(body) == 0 {
+      body = entry.S(NS_ATOM, "summary")
     }
-
-    if len(e.Body) == 0 {
-      e.Body = entry.S(ns, "summary")
-    }
-
-    if err := self.entryHandler(e); err != nil {
-      Error("[Feed]", err)
-      continue
-    }
-
-    self.entries[url] = true
+    self.registerEntry(url, entry.S(NS_ATOM, "title"), body)
   }
 }
 
-//
-func (self *Feed) extractAtomUrl(ns string, node *xmlx.Node) string {
-  links := node.SelectNodes(ns, "link")
+// Extracts the url from ATOM feed items.
+func (self *Feed) extractAtomUrl(node *xmlx.Node) string {
+  links := node.SelectNodes(NS_ATOM, "link")
 
   for _, link := range links {
     if link.As("", "rel") == "alternate" {
@@ -83,38 +86,24 @@ func (self *Feed) extractAtomUrl(ns string, node *xmlx.Node) string {
   if len(links) > 0 {
     return links[0].As("", "href")
   }
-
   return ""
 }
 
-//
+// Processes RSS feeds.
 func (self *Feed) processRss(node *xmlx.Node) {
-  ns := "*"
-  ch := node.SelectNode(ns, "channel")
+  ch := node.SelectNode(NS_RSS, "channel")
 
   if ch == nil {
     return
   }
-  self.Title = Titleize(node.S(ns, "title"))
+  self.Title = Titleize(node.S(NS_RSS, "title"))
 
-  for _, entry := range node.SelectNodes(ns, "item") {
-    url := entry.S(ns, "link")
+  for _, entry := range node.SelectNodes(NS_RSS, "item") {
+    url := entry.S(NS_RSS, "link")
 
-    if len(url) == 0 || self.entries[url] {
-      continue
+    if len(url) > 0 && !self.entries[url] {
+      self.registerEntry(url, entry.S(NS_RSS, "title"),
+        entry.S(NS_RSS, "description"))
     }
-
-    e := &db.Entry{
-      Url:   url,
-      Title: Titleize(entry.S(ns, "title")),
-      Body:  entry.S(ns, "description"),
-    }
-
-    if err := self.entryHandler(e); err != nil {
-      Error("[Feed]", err)
-      continue
-    }
-
-    self.entries[url] = true
   }
 }
